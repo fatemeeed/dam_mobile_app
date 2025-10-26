e6bf0264281d62526ba3f3f43f0337e9448a2038
fatemeeed <fatemedayian@yahoo.com>
2025-10-26 13:02:14 +0330

    add financialYearController and route

---
+++ app/Http/Controllers/API/Home/FinancialYearController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api\Home;
+
+use Illuminate\Http\Request;
+use App\Models\FinancialYear;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class FinancialYearController extends Controller
+{
+    public function index()
+    {
+        $years = FinancialYear::where('user_id', Auth::id())->latest()->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $years,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'title'       => 'nullable',
+                'start_date'  => 'required|date' ,
+                'end_date'    => 'required|date|after_or_equal:start_date'
+            ]
+        );
+
+
+         $financialYear = FinancialYear::create([
+            'user_id' => Auth::id(), // اگر احراز هویت داری
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+        ]);
+
+        return response()->json([
+             'success' => true,
+            'message' => 'سال مالی با موفقیت ثبت شد ',
+            'data' => $financialYear,
+        ]);
+
+
+
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth\AuthController;
-
-
+use App\Http\Controllers\Api\Home\FinancialYearController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
@@ -13,6 +12,14 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+
+
+    Route::prefix('financial-years')->group(function(){
+
+        Route::post('/store',[FinancialYearController::class,'store']);
+        Route::get('/',[FinancialYearController::class,'index']);
+
+    });
     // Route::get('/dashboard', [DashboardController::class, 'index']);
     // Route::post('/posts', [PostController::class, 'store']);
     // Route::put('/profile', [UserController::class, 'update']);
